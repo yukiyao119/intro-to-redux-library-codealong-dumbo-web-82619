5968a44996e200dd42b2a171524629a322a949e0
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-11-14 19:17:35 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
 
-const store = createStore(shoppingListItemReducer)
+const store = createStore(shoppingListItemReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+))
 
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
